323650dfd072971186140857d6f6a5954fa03a8b
aneesahDgeek <aneesahv92@gmail.com>
2023-02-17 19:46:55 -0800

    3 print alphabet c

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random number to the variable each time it is executed
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+
+printf("%d", n);
+
+printf("\n");
+return (0);
+}
