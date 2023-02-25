49ad5b2be21982575d516c6b245d657beb97d60d
aneesahDgeek <aneesahv92@gmail.com>
2023-02-25 06:03:17 -0800

    all done

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,19 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b && a > c)
-{
-largest - a;
-}
-else if (a > b && c > a)
-{
-largest = b;
-}
-else if (b > c)
-{
-largest = c;
-}
-return (largest);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else if (c >= b && c >= a)
+	{
+		largest = c;
+	}
+	return (largest);
 }
