66c45b4379f491153774516f7bfd8f470e4171cf
aneesahDgeek <aneesahv92@gmail.com>
2023-03-12 00:59:30 -0800

    putchar c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
-int _putchar(char c)
+int main(void)
 {
-    return putchar(c);
+
+printf("_putchar\n");
+
+return (0);
 }
